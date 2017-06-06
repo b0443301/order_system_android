ddfd9197a70ea8a916bc90a5645bb081cf8b5182
b0443301 <g02361081@gmail.com>
2017-06-06 11:32:47 +0800

    第十三版
    itemname、 itemprice、 iitemnumber的動態產生(從資料庫拿資料)
    按下spinner產生店家選項(第一格為預設值是spinner元件的特性)

--- app/src/main/java/com/example/user/order_system/DinnerActivity.java
+++ app/src/main/java/com/example/user/order_system/DinnerActivity.java
@@ -1,17 +1,14 @@
 package com.example.user.order_system;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -40,8 +37,8 @@ public class DinnerActivity extends AppCompatActivity {
     SelectItemData selectItemData;
     Spinner spinner;
     ArrayList<String> storenameList = new ArrayList<>();
+    ArrayList<String> itemList = new ArrayList<>();
     ArrayList<EditText> numberEDList = new ArrayList<>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,23 +47,7 @@ public class DinnerActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         itemlayout = (LinearLayout) findViewById(R.id.itemlayout);
-
-        spinner = (Spinner) findViewById(R.id.spinner);//制式寫法,把storenamelist寫道spinner
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, storenameList);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerArrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectItemData = new SelectItemData();//定義函式搜尋類別
-                selectItemData.execute();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        spinner = (Spinner) findViewById(R.id.spinner);
 
         selectStorenameData = new SelectStorenameData();//定義函式搜尋類別
         selectStorenameData.execute();
@@ -75,16 +56,7 @@ public class DinnerActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                TextView name = new TextView(DinnerActivity.this);
-//                name.setText("123");
-//                itemlayout.addView(name);
-//                TextView price = new TextView(DinnerActivity.this);
-//                price.setText("456");
-//                itemlayout.addView(price);
-//                EditText number = new EditText(DinnerActivity.this);
-//                number.setInputType(InputType.TYPE_CLASS_NUMBER);
-//                itemlayout.addView(number);
-//                numberEDList.add(number);
+
             }
         });
     }
@@ -108,11 +80,10 @@ public class DinnerActivity extends AppCompatActivity {
             }
         }
 
-
         @Override
         protected void onPostExecute(Void value) {
             super.onPostExecute(value);
-            selectStorenameData = null;
+            selectStorenameData = null;//
             // 背景工作處理完後需作的事
             try {
                 JSONObject jsonObject = new JSONObject(json);//結果錯誤時印出來
@@ -127,29 +98,34 @@ public class DinnerActivity extends AppCompatActivity {
                     try {
                         JSONObject jsonObject = new JSONObject(json);
                         JSONArray storename = jsonObject.getJSONArray("storename");//動態產生
+                        // debuger
+                        storenameList.add("!!!CHOICE!!!");//預設(defult)像請選擇按鍵
                         for (int i = 0; i < storename.length(); i++) {
                             storenameList.add(storename.getString(i));
-
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
-                    } finally {
+                    } finally {//要先從資料庫抓新加入的storename資料,才能觸發spinner,storenameList.add(storename.getString(i))之後
+                        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(DinnerActivity.this, android.R.layout.simple_spinner_item, storenameList);//制式寫法,把storenamelist寫道spinner
+                        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner.setAdapter(spinnerArrayAdapter);
+                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                itemList.clear();
+                                itemlayout.removeAllViews();
+                                if (position > 0) {
+                                    storename = storenameList.get(position);
+                                    selectItemData = new SelectItemData();//定義函式搜尋類別
+                                    selectItemData.execute();
+                                }
+                            }
 
+                            @Override
+                            public void onNothingSelected(AdapterView<?> parent) {
 
-//                        storenameACTV.setText(storename);
-//                        teleACTV.setText(telephone);
-//                        addressACTV.setText(address);
-//
-//                        for (int i = 0; i < nameList.size(); i++) {//namelist可以用.size()[因為是array才用,視情況],.length()
-//                            AutoCompleteTextView name = new AutoCompleteTextView(StoreMainActivity.this);
-//                            name.setText(nameList.get(i));//設定namelist拿到的值給name上(動態產生出來的)
-//                            itemlayout.addView(name);
-//                            AutoCompleteTextView price = new AutoCompleteTextView(StoreMainActivity.this);
-//                            price.setText(priceList.get(i));
-//                            itemlayout.addView(price);
-//
-//                            //nameACTVList.add(name);//新增nameACYV裡的值,在存檔,職責的部分,有些部分是U/I做,有的在伺服器幫我們做,如果不做sever會有疊加
-//                            //priceACTVList.add(price);
+                            }
+                        });
                     }
                 }
             }
@@ -178,7 +154,7 @@ public class DinnerActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void value) {
             super.onPostExecute(value);
-            selectStorenameData = null;
+            selectItemData = null;
             // 背景工作處理完後需作的事
             try {
                 JSONObject jsonObject = new JSONObject(json);//結果錯誤時印出來
@@ -186,40 +162,38 @@ public class DinnerActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             } finally {//finally一定會執行
-                if (result.equals("select_store_list_no_data")) {
-                    Toast.makeText(DinnerActivity.this, "select_store_list_no_data", Toast.LENGTH_LONG).show();
-                } else if (result.equals("select_store_list_success")) {
-                    Toast.makeText(DinnerActivity.this, "select_store_list_success", Toast.LENGTH_LONG).show();
+                if (result.equals("select_item_list_fail")) {
+                    Toast.makeText(DinnerActivity.this, "select_item_list_fail", Toast.LENGTH_LONG).show();
+                } else if (result.equals("select_item_list_no_data")) {
+                    Toast.makeText(DinnerActivity.this, "select_item_list_no_data", Toast.LENGTH_LONG).show();
+                } else if (result.equals("select_item_list_store_not_found")) {
+                    Toast.makeText(DinnerActivity.this, "select_item_list_store_not_found", Toast.LENGTH_LONG).show();
+                } else if (result.equals("select_item_list_success")) {
+                    Toast.makeText(DinnerActivity.this, "select_item_list_success", Toast.LENGTH_LONG).show();
                     try {
                         JSONObject jsonObject = new JSONObject(json);
-                        JSONArray storename = jsonObject.getJSONArray("storename");//動態產生
-                        for (int i = 0; i < storename.length(); i++) {
-                            storenameList.add(storename.getString(i));
-
+                        JSONArray item = jsonObject.getJSONArray("item");//動態產生
+                        for (int i = 0; i < item.length(); i++) {
+                            itemList.add(item.getString(i));
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
                     } finally {
-
-
-//                        storenameACTV.setText(storename);
-//                        teleACTV.setText(telephone);
-//                        addressACTV.setText(address);
-//
-//                        for (int i = 0; i < nameList.size(); i++) {//namelist可以用.size()[因為是array才用,視情況],.length()
-//                            AutoCompleteTextView name = new AutoCompleteTextView(StoreMainActivity.this);
-//                            name.setText(nameList.get(i));//設定namelist拿到的值給name上(動態產生出來的)
-//                            itemlayout.addView(name);
-//                            AutoCompleteTextView price = new AutoCompleteTextView(StoreMainActivity.this);
-//                            price.setText(priceList.get(i));
-//                            itemlayout.addView(price);
-//
-//                            //nameACTVList.add(name);//新增nameACYV裡的值,在存檔,職責的部分,有些部分是U/I做,有的在伺服器幫我們做,如果不做sever會有疊加
-//                            //priceACTVList.add(price);
+                        for (int i = 0; i < itemList.size(); i = i + 2) {//用itemList一次抓菜單名稱和菜單價錢,訂購數量由使用者自行輸入,不需從資料庫抓值給它
+                            TextView name = new TextView(DinnerActivity.this);
+                            name.setText(itemList.get(i));
+                            itemlayout.addView(name);
+                            TextView price = new TextView(DinnerActivity.this);
+                            price.setText(itemList.get(i + 1));
+                            itemlayout.addView(price);
+                            EditText number = new EditText(DinnerActivity.this);
+                            number.setInputType(InputType.TYPE_CLASS_NUMBER);
+                            itemlayout.addView(number);
+                            numberEDList.add(number);
+                        }
                     }
                 }
             }
         }
     }
-
 }
